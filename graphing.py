657ea8820eb8ca0789e50bb18d8e01edd0b7aec5
Chelsea <chelseaeverest7@gmail.com>
2023-05-23 14:52:15 -0400

    Decision trees

---
+++ graphing.py
@@ -0,0 +1,37 @@
+import seaborn as sns
+%matplotlib inline
+
+## Distribution
+# Histogram
+sns.histplot(ad_data['Age'],bins=30);
+
+# JointPlot
+sns.jointplot(x='total_bill',y='tip',data=tips,kind='scatter') #kind='hex','reg'
+
+# PairPlot
+sns.pairplot(tips)
+sns.pairplot(tips,hue='sex',palette='coolwarm')
+
+## Categorical
+# Barplot
+sns.barplot(x='sex',y='total_bill',data=tips) #estimator=np.std
+
+sns.countplot(x='sex',data=tips)
+
+# BoxPlot
+sns.boxplot(x="day", y="total_bill", data=tips,palette='rainbow') #hue="smoker"
+
+sns.violinplot(x="day", y="total_bill", data=tips,palette='rainbow') #hue='sex',split=True
+
+## Matrix
+# Heatmap
+sns.heatmap(tips.corr(),cmap='coolwarm',annot=True)
+
+flights.pivot_table(values='passengers',index='month',columns='year')
+
+#Clustermap
+sns.clustermap(pvflights)
+
+## Regressions Plots
+# Implot
+sns.lmplot(x='total_bill',y='tip',data=tips) #hue='sex'
